57273191a4c2ebe15c9d46884b1c570899a74234
Jose Hernandez <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-08-21 17:11:11 +0000

    creating a web

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,46 +1,91 @@
-import React from "react";
-const Cards =()=>{
-    return (
-        <>
-        <div className="row justify-content-center">
-            <h1 className="mx-auto p-4">Ver ahora...</h1>
-        <div className="card col-3 mx-2" style={{ width: '18rem' }}>
-        <img src="https://pics.filmaffinity.com/one_piece-647985949-large.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-            <h5 className="card-title">One Piece</h5>
-            <p className="card-text">Un joven pirata con poderes elásticos, Luffy, y su tripulación, los Sombrero de Paja, navegan en busca del legendario tesoro "One Piece". Enfrentan enemigos peligrosos y descubren misterios en un mundo lleno de islas exóticas. ¡Únete a la aventura más épica de los mares!</p>
-            <a href="#" className="btn btn-danger">Ver ahora mismo</a>
-        </div>
-    </div>
-    
-    <div className="card col-3 mx-2" style={{ width: '18rem' }}>
-        <img src="https://pics.filmaffinity.com/Dragon_Ball_Z_Serie_de_TV-697937148-large.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-            <h5 className="card-title">Dragon Ball</h5>
-            <p className="card-text">Goku y sus amigos defienden la Tierra de poderosos villanos en épicas batallas llenas de energía y transformaciones. ¡Aventuras cósmicas y combates explosivos te esperan en Dragon Ball Z!</p>
-            <a href="#" className="btn btn-danger">Ver ahora mismo</a>
-        </div>
-    </div>   
-    
-    <div className="card col-3 mx-2" style={{ width: '18rem' }}>
-        <img src="https://pics.filmaffinity.com/naruto-409273531-mmed.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-            <h5 className="card-title">Naruto</h5>
-            <p className="card-text">Naruto, un ninja con un poderoso espíritu y un sueño de ser Hokage, enfrenta enemigos mortales y descubre los secretos de su oscuro pasado. ¡Sumérgete en emocionantes batallas y una historia llena de amistad y determinación!</p>
-            <a href="#" className="btn btn-danger">Ver ahora mismo</a>
-        </div>
-    </div>
+import React, { useState } from "react";
 
-    <div className="card col-3 mx-2" style={{ width: '18rem' }}>
-        <img src="https://pics.filmaffinity.com/Bleach_Serie_de_TV-235942666-large.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-            <h5 className="card-title">Bleach</h5>
-            <p className="card-text">Ichigo Kurosaki, un adolescente con el poder de un Shinigami, lucha contra espíritus malignos y descubre oscuros secretos en el mundo espiritual. ¡Acompáñalo en batallas épicas y un viaje lleno de acción y misterio!</p>
-            <a href="#" className="btn btn-danger">Ver ahora mismo</a>
+const Cards = () => {
+    const cardsData = [
+        {
+            title: "One Piece",
+            description: "Un joven pirata con poderes elásticos...",
+            imgUrl: "https://pics.filmaffinity.com/one_piece-647985949-large.jpg"
+        },
+        {
+            title: "Dragon Ball",
+            description: "Goku y sus amigos defienden la Tierra...",
+            imgUrl: "https://pics.filmaffinity.com/Dragon_Ball_Z_Serie_de_TV-697937148-large.jpg"
+        },
+        {
+            title: "Naruto",
+            description: "Naruto, un ninja con un poderoso espíritu...",
+            imgUrl: "https://pics.filmaffinity.com/naruto-409273531-mmed.jpg"
+        },
+        {
+            title: "Bleach",
+            description: "Ichigo Kurosaki, un adolescente con el poder...",
+            imgUrl: "https://pics.filmaffinity.com/Bleach_Serie_de_TV-235942666-large.jpg"
+        },
+        {
+            title: "Attack on Titan",
+            description: "Eren y sus amigos luchan contra los titanes...",
+            imgUrl: "https://pics.filmaffinity.com/Ataque_a_los_Titanes_Serie_de_TV-890399004-large.jpg"
+        },
+        {
+            title: "My Hero Academia",
+            description: "Izuku Midoriya, un joven sin poderes...",
+            imgUrl: "https://pics.filmaffinity.com/My_Hero_Academia_Serie_de_TV-340675272-large.jpg"
+        },
+        {
+            title: "Record Of Ragnarok",
+            description: "Humanos legendarios como Adán y Jack el Destripador enfrentan...",
+            imgUrl: "https://pics.filmaffinity.com/Record_of_Ragnarok_Serie_de_TV-357029158-large.jpg"
+        },
+        {
+            title: "Fullmetal Alchemist",
+            description: "Los hermanos Elric buscan la piedra...",
+            imgUrl: "https://pics.filmaffinity.com/Fullmetal_Alchemist_Serie_de_TV-577038341-large.jpg"
+        }
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [direction, setDirection] = useState(""); 
+    
+
+    const cardsPerPage = 4;
+    const totalPages = Math.ceil(cardsData.length / cardsPerPage);
+
+    const goToNext = () => {
+        setDirection("next");
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % totalPages);
+    };
+
+    const goToPrevious = () => {
+        setDirection("prev");
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + totalPages) % totalPages);
+    };
+
+    const startIndex = currentIndex * cardsPerPage;
+    const currentCards = cardsData.slice(startIndex, startIndex + cardsPerPage);
+
+    return (
+        <div className="cards container">
+            <h1 className="text-center p-4">Ver ahora...</h1>
+            <div className={`row justify-content-center transition-${direction}`}>
+                {currentCards.map((card, index) => (
+                    <div key={index} className="card col-md-3 mx-2" style={{ width: '18rem' }}>
+                        <img src={card.imgUrl} className="card-img-top" alt={card.title} />
+                        <div className="card-body">
+                            <h5 className="card-title">{card.title}</h5>
+                            <p className="card-text">{card.description}</p>
+                            <a href="#" className="btn btn-danger">Ver ahora mismo</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="d-flex justify-content-between mt-4">
+                <button className="btn btn-outline-secondary" onClick={goToPrevious}>Previous</button>
+                <button className="btn btn-outline-secondary" onClick={goToNext}>Next</button>
+            </div>
         </div>
-    </div>   
-    </div>
-    </>
-    )
+    );
 };
-export default Cards
+
+export default Cards;
+
--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -7,21 +7,21 @@ const Jumbotron = () => {
         {
             title: "Novedades sobre el final de One Piece",
             text: "En el One Piece Day, celebrado este 10 y 11 de agosto, nos dieron pistas hacia donde va el final de la obra.",
-            buttonText: "Conoce más aquí"
-            
+            buttonText: "Conoce más aquí",
+            backgroundImage: "https://sm.ign.com/t/ign_es/gallery/t/the-one-pi/the-one-piece-concept-art_jkyj.600.jpg"
         },
         {
-            title: "Apoyanos a convertirnos en una plataforma de streaming top!",
+            title: "Apóyanos a convertirnos en una plataforma de streaming top!",
             text: "Con tus aportes podemos contratar las licencias de tus obras favoritas y compartirlas contigo gratuitamente.",
-            buttonText: "Conoce más aquí"
-            
+            buttonText: "Conoce más aquí",
+            backgroundImage: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2023/01/cuanto-cuesta-netflix-disney-hbo-max-prime-video-skyshowtime-resto-plataformas-streaming-2023-2927180.jpg"
         },
         {
             title: "Pronto tendremos Dragon Ball Daima",
             text: "This is the third jumbotron, showcasing another piece of information.",
-            buttonText: "Conoce más aquí"
-            
-        }
+            buttonText: "Conoce más aquí",
+            backgroundImage: "https://otakus-dream.com/wp-content/uploads/2024/05/Dragon-Ball-Daima-3.jpg"
+        },
     ];
 
     const goToNext = () => {
@@ -33,23 +33,31 @@ const Jumbotron = () => {
     };
 
     return (
-        <div className="container py-4">
-            <header className="pb-3 mb-4 border-bottom"></header>
-            <div className="p-5 mb-4 bg-body-tertiary rounded-3">
-                <div className="container-fluid py-5">
-                    <h1 className="display-5 fw-bold">{jumbotrons[currentJumbotron].title}</h1>
-                    <p className="col-md-8 fs-4">{jumbotrons[currentJumbotron].text}</p>
-                    <button className="btn btn-danger btn-lg" type="button">
-                        {jumbotrons[currentJumbotron].buttonText}
-                    </button>
-                </div>
+        <div className="jumbotron-container">
+            <div className="jumbotron-inner" style={{ transform: `translateX(-${currentJumbotron * 100}%)` }}>
+                {jumbotrons.map((jumbotron, index) => (
+                    <div
+                        key={index}
+                        className="jumbotron"
+                        style={{ backgroundImage: `url(${jumbotron.backgroundImage})` }}
+                    >
+                        <div className="container-fluid py-5 ">
+                            <h1 className="display-5 fw-bold">{jumbotron.title}</h1>
+                            <p className="col-md-8 fs-4">{jumbotron.text}</p>
+                            <button className="btn btn-primary btn-lg" type="button">
+                                {jumbotron.buttonText}
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
             <div className="d-flex justify-content-between">
-                <button className="btn btn-outline-secondary" onClick={goToPrevious}>Previous</button>
-                <button className="btn btn-outline-secondary" onClick={goToNext}>Next</button>
+                <button className="btn btn-outline-secondary m-2" onClick={goToPrevious}>Previous</button>
+                <button className="btn btn-outline-secondary m-2" onClick={goToNext}>Next</button>
             </div>
         </div>
     );
-}
+};
 
 export default Jumbotron;
+
